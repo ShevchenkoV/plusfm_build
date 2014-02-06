fa290e19edcda089136d4d9b223e23fc83268b8f
Vitaliy_S <elanceShevchenko@gmail.com>
2014-02-06 14:18:30 +0200

    epic refactor

--- login.php
+++
@@ -1,7 +0,0 @@
-<?PHP
- $host = "devgears.mysql.ukraine.com.ua";
- $user = "devgears_db";
- $password = "w6ZHm2qx";
- $db = mysql_connect($host, $user, $password);
- mysql_select_db("devgears_db", $db);
-?>
